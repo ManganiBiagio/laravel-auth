2bfb273b6720c3de9ce0aeb00aa911870d6c4eea
ManganiBiagio <biagiomangani@live.it>
2023-02-01 16:55:19 +0100

    creation controller dasboard and project +redirect

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,3 +29,15 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified'])
+    ->prefix("admin") // porzione di uri che verrà inserita prima di ogni rotta
+    ->name("admin.") // porzione di testo inserita prima del name di ogni rotta
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, "home"])->name('dashboard');
+        
+        
+
+        Route::resource("projects", ProjectController::class);
+       
+    });
